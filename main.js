8524102d6069d5d9f99a204ba7590a1e8588c062
André Rodrigues <afbr17@gmail.com>
2020-03-03 18:17:01 +0000

    Implement add books functionality

--- main.js
+++ main.js
@@ -1,8 +1,66 @@
 const addButtons = document.querySelectorAll(".add-button");
 const formContainer = document.getElementById("form-container");
+const tableBody = document.getElementById("table-body");
+const submit = document.getElementById("submit");
+
+let myLibrary = [];
+let lastBookNumber = 1;
 
 addButtons.forEach((button) => {
   button.addEventListener("click", () => {
     formContainer.style.display = "block";
   });
+});
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary(bookArgs) {
+  let book = new Book (...bookArgs);
+  myLibrary.push(book);
+}
+
+function render(){
+  let row = document.createElement("tr");
+  row.setAttribute("data-book", parseInt(lastBookNumber)+1);
+
+  if((parseInt(lastBookNumber)+1) % 2 === 0){ //Adds color to every other row
+    row.classList.add("color-row");
+  }
+
+  for (let item in myLibrary[myLibrary.length-1]){
+    let cell = document.createElement("td");
+    cell.append(myLibrary[myLibrary.length-1][item]);
+    row.append(cell);
+  }
+
+  tableBody.insertBefore(row, tableBody.firstChild);
+  lastBookNumber = tableBody.firstElementChild.getAttribute("data-book");
+}
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  
+  let form = document.querySelector("form");
+  let bookArgs = [];
+
+  for (let element of form.elements){
+    if (element.id === "read"){
+      element.checked ? bookArgs.push ("Yes") : bookArgs.push ("No");
+      element.checked = false;
+    }else{
+      bookArgs.push (element.value);
+      if (element.id !== "submit"){
+        element.value = "";
+      }
+    }
+  }
+
+  formContainer.style.display = "none";
+  addBookToLibrary(bookArgs);
+  render();
 });
